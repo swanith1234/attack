e2d3734f5760d5342b8c3a380cad085a1b11252a
swanith1234 <swanithpidugu@gmail.com>
2024-12-08 20:39:52 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"attack","emailId":"praidss@gmail.com","phoneNo":"4305932958","projects":[{"title":"swanith","description":"swanith","technologies":["React","Node.js"],"repoLink":"swanith","_id":"6755b662502bafe5c09e02f5"}],"resume":"","experiences":[{"companyName":"swanith","logo":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733670006/evolumin/hcqshmwzjh8szgmcbbxz.png","role":"swanith","description":"swanith","technologiesUsed":[],"duration":"swanith","_id":"6755b662502bafe5c09e02f6"}],"techStacks":["React","Node.js","java"],"contactDetails":["www.github.com"],"codingProfiles":["www.geeksforgeeks.org"],"certifications":["https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png"],"achievements":[{"title":"swanith","description":"swanith","_id":"6755b662502bafe5c09e02f7"},{"title":"swanith","description":"swanith","_id":"6755b662502bafe5c09e02f8"},{"title":"swanith","description":"swanithswanith","_id":"6755b662502bafe5c09e02f9"}],"about":"swanith","profilePhoto":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733669989/evolumin/ryqzbizbdlmglsjrtonc.jpg","_id":"6755b662502bafe5c09e02f4","createdAt":"2024-12-08T15:08:18.151Z","updatedAt":"2024-12-08T15:08:18.151Z","__v":0}} />
+  </StrictMode>,
+)
